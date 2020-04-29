93fd5245d38866764f837f78540c25aa373f484b
BulletYuan <326583424@qq.com>
2020-04-29 13:47:43 +0800

    create bones project

---
+++ bones/src/components/FlexLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-layout"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class FlexLayout extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.flex-layout {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  display: -webkit-flex;
+}
+</style>
---
+++ bones/src/components/GridLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid-layout"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class GridLayout extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.grid-layout {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  display: -ms-grid;
+}
+</style>
